0119db6e4ab8b1b0ff2dec75d590b09c095ab941
acoramadan <laxmiwatimaani@gmail.com>
2025-06-20 14:23:52 +0900

    update

--- nlp_pipeline/embedding_oversample.py
+++ nlp_pipeline/embedding_oversample.py
@@ -1,18 +1,16 @@
 from sklearn.utils import resample
 import numpy as np
+from collections import Counter
 import pandas as pd
 
 class EmbeddingOversampler:
-    def __init__(self, label_col='label', target_label='ASD'):
-        self.label_col = label_col
-        self.target_label = target_label
+    def oversample(self, X_embed, X_ling, y):
+        assert len(X_embed) == len(X_ling) == len(y), "Mismatch input shape!"
 
-    def oversample(self, X_embed: np.ndarray, X_ling: np.ndarray, y: np.ndarray):
-        idx_minority = np.where(y == 1)[0]
-        idx_majority = np.where(y == 0)[0]
+        idx_minority = np.where(y == 1)[0]  
+        idx_majority = np.where(y == 0)[0]  
 
-        n_majority = len(idx_majority)
-        n_minority = len(idx_minority)
+        print("Before Oversampling:", Counter(y))
 
         X_embed_min = X_embed[idx_minority]
         X_ling_min = X_ling[idx_minority]
@@ -21,7 +19,7 @@ class EmbeddingOversampler:
         X_embed_os, X_ling_os, y_os = resample(
             X_embed_min, X_ling_min, y_min,
             replace=True,
-            n_samples=n_majority,
+            n_samples=len(idx_majority),
             random_state=42
         )
 
@@ -29,4 +27,5 @@ class EmbeddingOversampler:
         X_ling_final = np.vstack([X_ling[idx_majority], X_ling_os])
         y_final = np.concatenate([y[idx_majority], y_os])
 
+        print("After Oversampling :", Counter(y_final))
         return X_embed_final, X_ling_final, y_final
